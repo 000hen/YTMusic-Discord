c9fce4eaecf6ddfca3ccdc357696c8e58b90d080
000hen <42735043+000hen@users.noreply.github.com>
2021-07-13 13:08:56 +0800

    當音樂暫停時Discord Rich Presence 會顯示暫停
    這個程式碼只支援: 中文(台灣) (zh-TW)

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,19 +1,40 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    sendResponse({
-        response: "Message Received! (content)"
+window.addEventListener('load', e => {
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        sendResponse({
+            response: "Message Received! (content)"
+        });
+        if(document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0] != undefined) {
+        sendMessage();
+        }
     });
-	if(document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0] != undefined) {
-    sendMessage();
-	}
-});
+    var pause = false;
 
-function sendMessage() {
-	var songName = document.getElementsByClassName("title style-scope ytmusic-player-bar")[0].innerHTML;
-    var artistName = document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0].innerText;
-    var time = document.getElementsByClassName("time-info style-scope ytmusic-player-bar")[0].innerText.toString().split('/')[1];
-    chrome.runtime.sendMessage({
-        song: songName,
-        artist: artistName,
-        timeMax: time
+    var observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type == "attributes") {
+                var doc = document.getElementById("play-pause-button").getAttribute("title");
+                if (doc === "暫停") pause = false;
+                if (doc === "播放") pause = true;
+                sendMessage();
+            }
+        });
     });
-}
+
+    observer.observe(document.getElementById("play-pause-button"), {
+        attributes: true,
+    });
+
+    function sendMessage() {
+        var songName = document.getElementsByClassName("title style-scope ytmusic-player-bar")[0].innerText;
+        var artistName = document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0].innerText;
+        var time = document.getElementsByClassName("time-info style-scope ytmusic-player-bar")[0].innerText.toString().split('/')[1];
+        var nowTime = Number(document.getElementById("progress-bar").getAttribute("value")) * 1000;
+        chrome.runtime.sendMessage({
+            song: songName,
+            artist: artistName,
+            timeMax: time,
+            nowTime: nowTime,
+            isPause: pause
+        });
+    }
+})
