3aefc0b3528ec8c3e2074edc94053411fe2a8c6c
000hen <42735043+000hen@users.noreply.github.com>
2021-07-15 14:56:09 +0800

    Make the app can block the multi-request.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const client = require('discord-rich-presence')('864326540736331778'); //我建議使用我的ID
+const client = require('discord-rich-presence')('864326540736331778');
 const express = require('express');
 var song = 'Waiting for music...';
 var artist = 'No Artist';
 const app = express();
 var tempTime = '0:00';
+var songPause = true;
 
 console.log('Starting YouTubeMusicDiscordRichPresence...')
 update(song,artist);
@@ -12,16 +13,23 @@ app.use(express.json());
 app.post("/", (request, response) => {
   let content = request.body;
 
+  if (content.isPause === songPause && content.song === song) {
+    response.sendStatus(202);
+    return;
+  }
+
   tempTime = content.timeMax.replace(' ', '');
   song = content.song
   artist = content.artist.replace(/\n/gm, "");
 
   if (content.isPause) {
+    songPause = true;
     console.log(`Pauseing now ${content.song}, by ${artist}, Time: ${content.nowTime}/${timeToMilli(content.timeMax.replace(' ', '')) - Date.now()}`);
     update(content.song, artist, Date.now());
     response.sendStatus(200);
     return;
   }
+  songPause = false;
 
   console.log(`Playing now ${content.song}, by ${artist}, Time: ${content.nowTime}/${timeToMilli(content.timeMax.replace(' ', '')) - Date.now()}`);
   update(content.song, artist,Date.now(), timeToMilli(content.timeMax.replace(' ', '')) - content.nowTime);
